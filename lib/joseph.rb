a4c5909d0e4aae6efb0a8f8d662fb1fb41014577
Martin Schürrer <martin@schuerrer.org>
2010-12-10 12:11:59 +0100

    Passing all specs

--- lib/joseph.rb
+++ lib/joseph.rb
@@ -1,3 +1,32 @@
+require 'ostruct'
+
 module Joseph
-  # Your code goes here...
+  def self.included(base)
+    if base.class == Module
+      base.extend self
+    else
+      super
+    end
+  end
+
+  def config
+    @config ||= OpenStruct.new
+  end
+
+  def configure
+    yield config
+  end
+
+  def [](key)
+    config.send key
+  end
+
+  # Lifted from http://github.com/stephencelis/app/blob/master/lib/app.rb#L69-76
+  def method_missing(method, *args, &block)
+    super if (key = method.to_s).end_with?("=")
+    boolean = key.chomp!("?")
+    value   = self[key]
+    value   = value.call(*args, &block) if value.respond_to?(:call)
+    boolean ? !!value : value
+  end
 end
--- spec/joseph_spec.rb
+++ spec/joseph_spec.rb
@@ -1,3 +1,32 @@
+require 'joseph'
+
 describe Joseph do
-  pending 'features'
+  describe 'adding it to a class' do
+    class A
+      include Joseph
+    end
+
+    let(:a) { A.new }
+
+    it 'includes Joseph' do
+      a.configure do |config|
+        config.hans = 'herbert'
+      end
+      a.hans.should == 'herbert'
+    end
+  end
+
+  describe 'adding it to a module' do
+    module M
+      include Joseph
+    end
+
+    it 'includes Joseph' do
+      M.configure do |config|
+        config.hans = 'herbert'
+      end
+      M.hans.should == 'herbert'
+    end
+  end
+
 end
